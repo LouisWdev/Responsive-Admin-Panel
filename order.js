fa47fe739f61c6b88db2f0891d02c003b0bfcdda
LouisWackenier <Louis.wackenier@gmail.com>
2023-07-22 17:21:17 +0200

    New project. A responsive Dashboard. Working on the sidebar

---
+++ order.js
@@ -0,0 +1,18 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Declined' 
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        paymentStatus: 'Active' 
+    },
+]
